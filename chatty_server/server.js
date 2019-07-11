85836f33e88e9ed66cac86d436103373439ada0b
Harrison2301 <harrisonwinters2301@gmail.com>
2019-07-11 23:04:35 +0000

    re-added chatty_server

---
+++ chatty_server/server.js
@@ -0,0 +1,68 @@
+// server.js
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+
+// Set the port to 3001
+const PORT = 3001;
+
+
+// Create a new express server
+const server = express()
+// Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static('public'))
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${PORT}`));
+
+// Create the WebSockets server
+const wss = new SocketServer({ server });
+
+// Set up a callback that will run when a client connects to the server
+// When a client connects they are assigned a socket, represented by
+// the ws parameter in the callback.
+let clientCount = 0;
+wss.on('connection', (ws) => {
+  clientCount++
+  wss.clients.forEach(function (users){
+    users.send(JSON.stringify({counter:clientCount}))
+  })
+  console.log(clientCount)
+  console.log('Client connected');
+
+  ws.on('message', (data) => {
+    wss.clients.forEach((client) => {
+      const {
+        username, content, id, type, oldName,
+      } = JSON.parse(data);
+      console.log(content);
+      if (type === 'newMessage') {
+        console.log();
+        const newID = {
+          id,
+          content,
+          username: username || 'Anonymous',
+          type,
+          counter:clientCount
+        };
+        client.send(JSON.stringify(newID));
+      } else if (type === 'postNotification') {
+        const notif = {
+          name: username,
+          id,
+          content: `${oldName} has updated their name to ${username}`,
+          type: 'incomingNotification',
+          counter:clientCount
+          
+        };
+        wss.clients.forEach((users) => {
+          users.send(JSON.stringify(notif));
+        });
+      }
+    });
+  });
+  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    clientCount--
+    console.log(clientCount)
+  });
+});

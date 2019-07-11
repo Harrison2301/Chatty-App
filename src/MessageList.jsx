6f601182fc95d9aa399a85655b4bda6ba2c40d16
Harrison2301 <harrisonwinters2301@gmail.com>
2019-07-11 05:15:07 +0000

    update websocket to accept message

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -2,14 +2,20 @@ import React, {Component} from 'react';
 import Message from './Message.jsx';
 import Notification from './Notification.jsx';
 
-class MessageList extends Component {
-  render() {
-      const messages = this.props.messages.map((value) => <Message key={value.id} name={value.username} contentMessage={value.content}/>);
-    return (
-        <main className="messages">
-            {messages}
-      </main>
-    );
-  }
+function MessageList(props) {
+  return (
+    <main className= "messages">
+      {props.messages.map(message => {
+        if (message.type === "newMessage"){
+          return <Message key={message.id} message={message}/>
+        } else {
+          return <Notification key={message.id} message={message.content}/>
+        }
+
+      })
+      }
+  
+  </main>
+  )
 }
 export default MessageList;
